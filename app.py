8a197b7945159719cc92d033b387d4d1cf4a9c38
かわさき <kawasaki@kawasakinoMacBook-Pro.local>
2022-02-09 05:39:32 +0900

    first commit

--- app.py
+++ app.py
@@ -1,17 +1,49 @@
 # 必要モジュールインポート
 from flask import Flask, render_template, request, redirect
+from numpy import void
+from sqlalchemy import null
 import scraping
+import re
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route('/scraping', methods=["GET"])
+@app.route('/scraping', methods=["POST"])
 def run_scraping():
 
-    result = scraping.scraping()
+    pokename1 = request.form['pokename1']
+    pokename2 = request.form['pokename2']
+    pokename3 = request.form['pokename3']
+    pokename4 = request.form['pokename4']
+    pokename5 = request.form['pokename5']
+
+    all5 = scraping.scraping()
+
+    result = []
+    if pokename1 != null:
+        for i in all5:
+            if pokename1 in i:
+                result.append(i)
+    if pokename2 != null:
+        for i in all5:
+            if pokename2 in i:
+                result.append(i)
+    if pokename3 != null:
+        for i in all5:
+            if pokename3 in i:
+                result.append(i)
+    if pokename4 != null:
+        for i in all5:
+            if pokename4 in i:
+                result.append(i)
+    if pokename5 != null:
+        for i in all5:
+            if pokename5 in i:
+                result.append(i)
+
 
     return render_template('result.html', result=result)
 
--- scraping.py
+++ scraping.py
@@ -3,63 +3,29 @@ from bs4 import BeautifulSoup
 
 
 def scraping():
-    # くら寿司メニューのURLを変数に代入
-    URL = 'https://www.kurasushi.co.jp/menu/?area=area0'
+    # ポケモン図鑑のURLを変数に代入
+    URL = 'https://yakkun.com/swsh/zukan/'
 
     # URLの取得
     results = requests.get(URL)
+    results.encoding = 'EUC-JP'
 
     # HTML、XMLの要素をパースする
     data1 = BeautifulSoup(results.text, 'html.parser')
 
     # クラスを指定し、見出しを取得する
-    data2 = data1.find(id="menu02")
-    data3 = data1.find(id="menu03")
-    data4 = data1.find(id="menu04")
-    data7 = data1.find(id="menu07")
-    data8 = data1.find(id="menu08")
+    data2 = data1.find(class_="pokemon_list")
 
     # それぞれのメニューを抽出
-    rows = data2.find_all("h4")
+    rows = data2.find_all("a")
 
     # 配列の値をfor文とprint文で出力、rowsの定義変更
-    rows2 = []
+    pokeArray = []
     for row in rows:
-        print(row.getText())
-        rows2.append(row.getText())
-    print('-------------------')
+        if len(row.getText()) == 5:
+            pokeArray.append(row.getText())
 
-    rows = data3.find_all("h4")
-    rows3 = []
-    for row in rows:
-        print(row.getText())
-        rows3.append(row.getText())
-    print('-------------------')
+    return pokeArray
 
-    rows = data4.find_all("h4")
-    rows4 = []
-    for row in rows:
-        print(row.getText())
-        rows4.append(row.getText())
-    print('-------------------')
-
-    rows = data7.find_all("h4")
-    rows7 = []
-    for row in rows:
-        print(row.getText())
-        rows7.append(row.getText())
-    print('-------------------')
-
-    rows = data8.find_all("h4")
-    rows8 = []
-    for row in rows:
-        print(row.getText())
-        rows8.append(row.getText())
-    print('-------------------')
-
-    sumRows = rows2 + rows3 + rows4 + rows7 + rows8
-
-    #予定：正規表現を外す
-
-    return sumRows
+    
     
